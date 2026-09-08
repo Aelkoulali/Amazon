60de5a6bb4c5c5394172fd9aaa0f8913c674b445
Abdellah <abdellah.elkoulali@gmx.fr>
2026-09-08 16:22:46 +0200

    Add product data structure to amazon.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+const products = [{
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name:"Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+        stars: 4.5,
+        count: 127
+    },
+    priceCents: 1090
+}, {
+
+}
+];
