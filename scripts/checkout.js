26a01089fdc26636d8a177f8fd329da227e51e15
Abdellah <abdellah.elkoulali@gmx.fr>
2026-09-10 17:33:32 +0200

    Fix formatting and indentation in checkout.js for improved readability

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,9 +15,9 @@ cart.forEach((cartItem) => {
     }
   });
 
- cartSummaryHTML +=
+  cartSummaryHTML +=
 
-  `
+    `
   <div class="cart-item-container 
     js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">
@@ -92,13 +92,13 @@ cart.forEach((cartItem) => {
 document.querySelector(".js-order-summary").innerHTML = cartSummaryHTML;
 
 document.querySelectorAll(".js-delete-link")
-.forEach((link) => { 
- link.addEventListener("click", () => {
- const productId = link.dataset.productId;
- removeFromCart(productId);
+  .forEach((link) => {
+    link.addEventListener("click", () => {
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
 
- const container = document.querySelector(`.js-cart-item-container-${productId}`);
- container.remove();   
-});
+      const container = document.querySelector(`.js-cart-item-container-${productId}`);
+      container.remove();
+    });
 
-});
+  });
